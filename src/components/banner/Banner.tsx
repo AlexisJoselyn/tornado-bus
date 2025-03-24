c9a5589e660a221ce5f1361a83718b25b05352a9
AlexisJoselyn <pfernandez.alexis@gmail.com>
2025-03-24 11:53:51 -0500

    style

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -2,7 +2,7 @@ import bannerImage from '../../assets/banner.png';
 
 export const Banner = () => {
   return (
-    <div className="relative w-full h-80 md:h-80 lg:h-96 xl:h-[500px] -mb-8">
+    <div className="relative w-full h-80 md:h-80 lg:h-96 -mb-8">
       <img
         src={bannerImage}
         alt="Promoción de viajes"
